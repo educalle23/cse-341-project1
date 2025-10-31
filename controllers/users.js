ab80cea81e30bdb4d62657a0152ee7fb34a72cb5
educalle23 <edu.lds.23@gmail.com>
2025-10-31 11:36:50 -0500

    try and catch added

--- controllers/users.js
+++ controllers/users.js
@@ -3,19 +3,61 @@ const { ObjectId } = require("mongodb");
 
 console.log(ObjectId);
 
+// const getAll = async (req, res) => {
+//   const result = await mongodb.getDB().db().collection("users").find();
+//   result.toArray().then((users) => {
+//     res.setHeader("Content-Type", "application/json");
+//     res.status(200).json(users);
+//   });
+// };
+
 const getAll = async (req, res) => {
-  const result = await mongodb.getDB().db().collection("users").find();
-  result.toArray().then((users) => {
+  try {
+    const result = await mongodb.getDB().db().collection("users").find();
+    const users = await result.toArray();
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(users);
-  });
+  } catch (error) {
+    console.error("Error en getAll:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 };
+
+// const getById = async (req, res) => {
+//   console.log("ID recibido:", req.params.id);
+//   console.log("Longitud:", req.params.id?.length);
+//   console.log("Tipo:", typeof req.params.id);
+//   const userId = new ObjectId(req.params.id);
+//   result = await mongodb.getDB().db().collection("users").find({ _id: userId });
+//   result.toArray().then((users) => {
+//     res.setHeader("Content-Type", "application/json");
+//     res.status(200).json(users[0]);
+//   });
+// };
+
 const getById = async (req, res) => {
-  result = await mongodb.getDB().db().collection("users").find({ _id: userId });
-  result.toArray().then((users) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ error: "ID inválido" });
+    }
+
+    const userId = new ObjectId(req.params.id);
+    const user = await mongodb
+      .getDB()
+      .db()
+      .collection("users")
+      .findOne({ _id: userId });
+
+    if (!user) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
     res.setHeader("Content-Type", "application/json");
-    res.status(200).json(users[0]);
-  });
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error en getById:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 };
 
 module.exports = {
